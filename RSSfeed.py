f07d29f07caabd22a55da35bc63739e5a843fd3e
nsrchemie <nsrchemie@gmail.com>
2016-01-20 14:29:29 -0500

    documentation and function description

--- RSSfeed.py
+++ RSSfeed.py
@@ -2,6 +2,19 @@ import feedparser
 import webbrowser
 import datetime
 def ChemJ_Feed():
+	"""A function that prompts the user to choose from a premade list of Phytochemical/Natural Product articles listed by acronym
+	Upon entering a journal a list of articles from the current RSS feed is returned.  The user is then prompted if they would
+	like to save the file.  Afterwards another prompt asks the user to type the first few words of the title of an article that 
+	interests them.  A web browser is then opened to the article page.
+
+	PARAMETERS
+	==========
+	Input:
+	None
+
+	Output:
+	
+	"""
 	RSS = {'JEP':'http://rss.sciencedirect.com/publication/science/5084', 'JNP':'https://feeds.feedburner.com/acs/jnprdf'}
 	journal = input("Which journal would you like to view?" + '\n' + str([key for key in RSS.keys()]))
 	titles = [text.title.encode('ascii','replace').decode('utf-8','ignore') + '\n' + text.link for text in feedparser.parse(RSS[journal]).entries]
