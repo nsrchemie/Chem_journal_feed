0a6805b30e7c83d0aabb89944081e5f17a75a444
nsrchemie <nsrchemie@gmail.com>
2016-01-20 14:02:16 -0500

    First commit

---
+++ RSSfeed.py
@@ -0,0 +1,16 @@
+import feedparser
+import webbrowser
+
+def ChemJ_Feed():
+	RSS = {'JEP':'http://rss.sciencedirect.com/publication/science/5084', 'JNP':'https://feeds.feedburner.com/acs/jnprdf'}
+	journal = input("Which journal would you like to view?" + '\n' + str([key for key in RSS.keys()]))
+	titles = [text.title.encode('ascii','replace').decode('utf-8','ignore') + '\n' + text.link for text in feedparser.parse(RSS[journal]).entries]
+	print('\n\n'.join(titles))
+	options = input('\n' + 'Would you like to view a particular title in the browser? Type the first few words of the article.')
+	for text in titles:
+		if text.startswith(options):
+			webbrowser.open(text[text.find('http:'):])
+
+
+if __name__ == "__main__":
+	ChemJ_Feed()
